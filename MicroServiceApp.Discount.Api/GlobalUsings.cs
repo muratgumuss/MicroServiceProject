a28835120d2faa75ea7ad86acce1de2ca326447e
Murat Gümüş <muratgumus05@gmail.com>
2025-11-19 15:08:17 +0300

    discount config settings

---
+++ MicroServiceApp.Discount.Api/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿#region
+
+global using AutoMapper;
+global using MediatR;
+global using Microsoft.EntityFrameworkCore;
+global using MicroServiceApp.Shared;
+global using MicroServiceApp.Shared.Extensions;
+global using MassTransit;
+global using System.Net;
+global using FluentValidation;
+
+#endregion
---
+++ MicroServiceApp.Discount.Api/Program.cs
@@ -0,0 +1,23 @@
+using MicroServiceApp.Discount.Api;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddVersioningExt();
+builder.Services.AddCommonServiceExt(typeof(DiscountAssembly));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.Run();
