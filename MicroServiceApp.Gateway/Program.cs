1832b45e2b99bfad2a2beead11c181f9cd1b5bc3
Murat Gümüş <muratgumus05@gmail.com>
2025-12-09 16:55:23 +0300

    yarp api gateway

---
+++ MicroServiceApp.Gateway/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+
+//builder.Services.AddAuthenticationAndAuthorizationExt(builder.Configuration);
+var app = builder.Build();
+app.MapReverseProxy();
+app.MapGet("/", () => "YARP (Gateway)");
+//app.UseAuthentication();
+//app.UseAuthorization();
+app.Run();
